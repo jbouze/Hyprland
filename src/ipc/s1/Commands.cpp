8340818dab3b1298abc6c085c57ab54b40b7d66d
Jean-Baptiste Bertin <jbouze@gmail.com>
2026-08-12 14:39:32 +0200

    Expose 'enabled' status of pointers (mice and touchpads) to hyperctl

--- src/ipc/s1/Commands.cpp
+++ src/ipc/s1/Commands.cpp
@@ -759,10 +759,12 @@ static std::string devicesRequest(eHyprCtlOutputFormat format, std::string reque
         "address": "0x{:x}",
         "name": "{}",
         "defaultSpeed": {:.5f},
-        "scrollFactor": {:.2f}
+        "scrollFactor": {:.2f},
+        "enabled": "{}"
     }},)#",
                 rc<uintptr_t>(m.get()), escapeJSONStrings(m->m_hlName),
-                m->aq() && m->aq()->getLibinputHandle() ? libinput_device_config_accel_get_default_speed(m->aq()->getLibinputHandle()) : 0.f, m->m_scrollFactor.value_or(-1));
+                m->aq() && m->aq()->getLibinputHandle() ? libinput_device_config_accel_get_default_speed(m->aq()->getLibinputHandle()) : 0.f, m->m_scrollFactor.value_or(-1),
+                m->m_connected);
         }
 
         trimTrailingComma(result);
@@ -866,9 +868,9 @@ static std::string devicesRequest(eHyprCtlOutputFormat format, std::string reque
         result += "mice:\n";
 
         for (auto const& m : g_pInputManager->m_pointers) {
-            result += std::format("\tMouse at {:x}:\n\t\t{}\n\t\t\tdefault speed: {:.5f}\n\t\t\tscroll factor: {:.2f}\n", rc<uintptr_t>(m.get()), m->m_hlName,
+            result += std::format("\tMouse at {:x}:\n\t\t{}\n\t\t\tdefault speed: {:.5f}\n\t\t\tscroll factor: {:.2f}\n\t\t\tenabled: {}\n", rc<uintptr_t>(m.get()), m->m_hlName,
                                   (m->aq() && m->aq()->getLibinputHandle() ? libinput_device_config_accel_get_default_speed(m->aq()->getLibinputHandle()) : 0.f),
-                                  m->m_scrollFactor.value_or(-1));
+                                  m->m_scrollFactor.value_or(-1), m->m_connected);
         }
 
         result += "\n\nKeyboards:\n";
